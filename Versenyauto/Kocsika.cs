a849af3ba777725643bf6fb0a8af6d11ac92835c
Fekete János Dávid <janosdavidfekete@gmail.com>
2024-03-27 08:58:35 +0100

    Kész a feladat

---
+++ Versenyauto/Kocsika.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Versenyauto
+{
+    public class Kocsika
+    {
+        public string Make;
+        public string Model;
+        public int Year;
+        public int Speed { get; set; }
+
+        public Kocsika(string make, string model, int year, int speed)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Speed = 0;
+            Sebesseg_nov();
+        }
+
+        public void Sebesseg_nov()
+        {
+            if (Speed == 0)
+            {
+                int sebesseg = new Random().Next(1,251);
+                Speed = sebesseg;
+            }
+        }
+
+        public override string? ToString()
+        {
+            return $"Az én kocsim: {Make}, Modell: {Model}, Évjárat: {Year}, Sebessége: {Speed}";
+        }
+    }
+}
---
+++ Versenyauto/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Versenyauto
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Kocsi kocsi = new Kocsi("Suzuki", "Swift", 1993, 0);
+            Console.WriteLine(kocsi);
+        }
+    }
+}
